ca8a59cd7e0cc85ba9252a217f5ebb65154396f3
Emmanuel VAISSE <emmanuel.vaisse@boursorama.fr>
2018-06-05 12:24:58 +0200

    update

--- index.js
+++ index.js
@@ -2,13 +2,55 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 
+
+`
+        ____      __
+       /  _/___  / /__________
+       / // __ \/ __/ ___/ __ \
+     _/ // / / / /_/ /  / /_/ /
+    /___/_/ /_/\__/_/   \____/
+
+
+    "In computer science, future, promise, delay, and deferred refer to constructs 
+     used for synchronizing program execution in some concurrent programming languages. "
+            -- Wiki PEDIA
+
+Promises : 
+
+    - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    - https://promisesaplus.com
+    - https://github.com/stefanpenner/es6-promise
+    - https://en.wikipedia.org/wiki/Futures_and_promises
+    - https://medium.com/@kangzeroo/quick-story-about-javascript-promises-31b4e76ed0cd
+
+History : 
+
+    - Fight the "callback-hell" syndrom 
+        - https://cdn-images-1.medium.com/max/800/1*UN325lO3m7E30D-KdpiKAw.png
+        - http://callbackhell.com
+        - http://thecodebarbarian.com/2015/03/20/callback-hell-is-a-myth
+    - The term "promise" was proposed in 1976 by Daniel P. Friedman and David Wise
+    - 2011 : JQuery Deferred Object (A promise-like)
+    - The idea : https://mdn.mozillademos.org/files/15911/promises.png
+    - "Pure" JS implementation first. Why native promises ? => Better error reporting, better warnings
+    - Where's promises fails ? Replay -> async reactive programming
+    - Async / Await syntax
+
+`
+
+var ajax = function (url) {
+    return fetch(url).then(function (res) {
+        return res.json();
+    });
+};
+
 // setTimeout(function () {
 
 //     console.log('terminé');
 
 // }, 1000); // 1sec
 
-// fetch("https://httpbin.org/get").then(function (response) {
+// ajax("https://httpbin.org/get").then(function (response) {
 //     return response.json();
 // }).then(function (json) {
 //     console.log(json);
@@ -16,7 +58,7 @@ require('isomorphic-fetch');
 // }).then(function (result) {
 //     console.log(result);
 // });
-
+/*
 
 Promise.all([
     fetch("https://httpbin.org/get?foo=1"),
@@ -24,24 +66,41 @@ Promise.all([
     fetch("https://httpbin.org/get?foo=3"),
 ]).then(function (results) {
 
-    return Promiseresults.map(function (response) { 
+    return Promise.all(results.map(function (response) { 
         return response.json(); 
-    });
-
-    
+    }));
+   
 
 }).then(function (results) {
 
     console.log(results);
 
-})
+}) */
 
-
+/*
 
 Promise.resolve(1);
-Promise.resolve(fetch("https://httpbin.org/get"));
+Promise.resolve(ajax("https://httpbin.org/get"));
 Promise.resolve(Promise.reject("arghhhh"));
 
+*/
+
+/*
+        __________  ____  ____  ____     __  _____    _   ______  __    _____   ________
+       / ____/ __ \/ __ \/ __ \/ __ \   / / / /   |  / | / / __ \/ /   /  _/ | / / ____/
+      / __/ / /_/ / /_/ / / / / /_/ /  / /_/ / /| | /  |/ / / / / /    / //  |/ / / __
+     / /___/ _, _/ _, _/ /_/ / _, _/  / __  / ___ |/ /|  / /_/ / /____/ // /|  / /_/ /
+    /_____/_/ |_/_/ |_|\____/_/ |_|  /_/ /_/_/  |_/_/ |_/_____/_____/___/_/ |_/\____/
+
+ */
+
+Promise.all([
+    ajax("https://httpbin.org/get"),
+    ajax("https://httpbin.org/status/500"),
+    // ajax("https://httpbin.org/status/500").catch(function (error) { return {}; }),
+]).then(function (results) {
+    console.log(results);
+});
 
 /*
     Promise.all()
@@ -51,4 +110,6 @@ Promise.resolve(Promise.reject("arghhhh"));
     Promise.race()
     Promise.reject()
     Promise.resolve()
+
+    async / await
 */
